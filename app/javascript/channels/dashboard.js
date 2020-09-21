75ccac271a2965e436c8541809839f27882ec2e3
miguel chirino <mchirinot@gmail.com>
2020-09-21 13:28:22 +0200

    dashboard-styling

--- app/javascript/channels/dashboard.js
+++ app/javascript/channels/dashboard.js
@@ -1,16 +1,14 @@
-// function display() {
-//     const b = document.getElementById("show-btn");
-//     const i = document.getElementById("order-box");
-//     b.addEventListener("click", function(){
-//       console.log("hola")
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-//       if (i.style.display === "none") {
-//               i.style.display = "block";
-//           } else {
-//                   i.style.display = "none";
-//               }
-        
-//       })
-//     }
-
-//   export {display};
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
