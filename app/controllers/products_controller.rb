2d526cd07db05217eaffc89cbf6b098e59f3acec
Martin Bettaglio <martin.bettaglio@gmail.com>
2020-09-22 14:08:39 +0200

    index product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+
+  def index
+    if params[:query].present?
+
+      @products = Product.search_for(params[:query])
+    else
+      @products = Product.all
+    end
+  end
+
+
+
+
 end
