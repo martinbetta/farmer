aa9ab756a07b693883aaa53aa25565756c26c942
kevin sauvage <kevinsauvage@outlook.com>
2020-09-29 09:03:45 +0200

    stripe

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,6 +3,7 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(status: 'paid')
+    order.user.orders.push(Order.create(status: "new"))
   end
 
 end
