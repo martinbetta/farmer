42fe542caa074b0ac809a0fde2d788b64d9c3d52
kevin sauvage <kevinsauvage@outlook.com>
2020-10-01 12:54:28 +0200

    fix bur

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << "24e0b513f36a.ngrok.io"
+  config.hosts << "efd6e8c6b939.ngrok.io"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
