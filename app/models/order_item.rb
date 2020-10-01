cea128c70c68a9757f2aab918d378a05ea02fc1b
gerrit pieter van t riet <geertriet@gmail.com>
2020-10-01 10:24:10 +0200

    subtotal order overview page fixed

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -10,4 +10,9 @@ class OrderItem < ApplicationRecord
   def total_price
     ActionView::Base.new.humanized_money(self.price * self.quantity)
   end
+
+  # def total_price_money
+  #   @money = Money.new(self.price * self.quantity)
+  #   ActionView::Base.new.humanized_money_with_symbol(@money, 'EUR'))
+  # end
 end
